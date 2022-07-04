f66c489b7cb5a1adaf09e3cf539c1018991e45b5
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-07-04 19:23:49 +0300

    Added a shopping cart barButtonItem to the ProductsViewController.
    Added func configureCartBarButtonItem and cartButtonTapped to ProductsViewController.

--- Shopping Cart/UI/ViewControllers/ProductsViewController.swift
+++ Shopping Cart/UI/ViewControllers/ProductsViewController.swift
@@ -8,16 +8,37 @@
 import UIKit
 
 final class ProductsViewController: UIViewController {
-
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureView()
+        configureCartBarButtonItem()
     }
+    
+    //MARK: - Private methods
     
     private func configureView() {
         view.backgroundColor = .systemBackground
         title = "Товары"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureCartBarButtonItem() {
+        let configuration = UIImage.SymbolConfiguration(pointSize: 20)
+        let cartButton = UIBarButtonItem(
+            image: SFSymbols.cart?.withConfiguration(configuration),
+            style: .plain,
+            target: self,
+            action: #selector(cartButtonTapped)
+        )
+        navigationItem.rightBarButtonItem = cartButton
+    }
+    
+    //MARK: - Actions
+    @objc func cartButtonTapped() {
+        
     }
 }
