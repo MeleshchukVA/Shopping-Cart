c90ec0a58b71d7332fb1abb2873200251f2ca3e8
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-07-04 08:50:51 +0300

    Initial Commit

---
+++ Shopping Cart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping Cart
+//
+//  Created by Владимир Мелещук on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
